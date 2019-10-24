0537cab9877d9a7e80f3b14febb7ad7a95075755
Kora Loudermilk <koraloudermilk@gmail.com>
2019-10-24 13:44:23 -0400

    working

--- CppServer/main.cpp
+++ CppServer/main.cpp
@@ -5,6 +5,7 @@
 #pragma comment(lib, "ws2_32.lib")
 
 #define DEFAULT_PORT "3000"
+#define DEFAULT_BUFLEN 512
 
 int main()
 {
@@ -58,6 +59,15 @@ int main()
 		return 1;
 	}
 	
+
+
+	char str[INET_ADDRSTRLEN];
+
+	//inet_ntop(result->ai_family, result->ai_addr, str, INET_ADDRSTRLEN);
+
+	//std::cout << "on addr " << str << " on port " << DEFAULT_PORT << std::endl;
+	std::cout << "bound to port " << DEFAULT_PORT << " at address 127.0.0.1"<< std::endl;
+
 	freeaddrinfo(result);
 	
 	if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
@@ -69,10 +79,62 @@ int main()
 
 	//client socket
 	//TODO: expand for multiple clients
-	//https://docs.microsoft.com/en-us/windows/win32/winsock/accepting-a-connection
 
 	SOCKET ClientSocket;
+	ClientSocket = accept(ListenSocket, NULL, NULL);
+	if (ClientSocket == INVALID_SOCKET)
+	{
+		std::cerr << "invalid socket " << WSAGetLastError() << std::endl;
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+	std::cout << "connectd" << std::endl;
+	char recvbuf[DEFAULT_BUFLEN];
+	int recvResult, sendResult;
+	int recvbuflen = DEFAULT_BUFLEN;
 
+
+	//loop until connection is ended
+	do 
+	{
+		recvResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+		if (recvResult > 0)
+		{
+			std::string recmessage = std::string(&recvbuf[0], recvResult);
+			std::cout << recmessage;
+			sendResult = send(ClientSocket, recvbuf, recvResult, 0);
+			if (sendResult == SOCKET_ERROR)
+			{
+				std::cerr << "send failed: " << WSAGetLastError() << std::endl;
+				closesocket(ClientSocket);
+				WSACleanup();
+				return 1;
+			}
+		}
+		else if (recvResult == 0)
+		{
+			std::cout << "connection closing" << std::endl;
+		}
+		else {
+			std::cout << "recv failed: " << WSAGetLastError() << std::endl;
+			closesocket(ClientSocket);
+			WSACleanup();
+			return 1;
+		}
+	} while (recvResult > 0);
+
+	errresult = shutdown(ClientSocket, SD_SEND);
+	if (errresult == SOCKET_ERROR)
+	{
+		std::cerr << "shutdown failed " << WSAGetLastError() << std::endl;
+		closesocket(ClientSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	closesocket(ClientSocket);
+	WSACleanup();
 
 	return 0;
 }
